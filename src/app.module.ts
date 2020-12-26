92300a9ad4810068827a5c844ae2a1a0d9e2c5e1
현태수 <qvil@op.gg>
2020-12-26 10:31:05 +0900

    Add Database(MySQL) with Docker, TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { IpController } from './ip/ip.controller';
 import { RedirectController } from './redirect/redirect.controller';
-import { ProductsModule } from './products/products.module';
-import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from './typeorm.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, TypeOrmModule],
   controllers: [AppController, IpController, RedirectController],
   providers: [AppService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    return this.usersRepository.save(createUserDto);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: number): Promise<User> {
+    return this.usersRepository.findOne(id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  update(id: number, updateUserDto: UpdateUserDto) {}
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  // async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+  //   // let userToUpdate = await this.usersRepository.findOne
+  //   let userToUpdate = this.findOne(id);
+  //   userToUpdate = {
+  //     ...updateUserDto,
+  //   };
+
+  //   return await this.usersRepository.save(userToUpdate);
+  //   // return await this.usersRepository.update(id, updateUserDto);
+  // }
+
+  async remove(id: number): Promise<void> {
+    await this.usersRepository.delete(id);
   }
 }
