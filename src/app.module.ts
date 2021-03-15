1bfb7b3f558bb8579b6c5d2547c9038e787ed0c5
Taesu Hyeon <email@qvil.dev>
2021-03-16 01:27:40 +0900

    Change ormconfig to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
@@ -7,20 +7,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IpController } from './ip/ip.controller';
 import { RedirectController } from './redirect/redirect.controller';
-import { CatsModule } from './cats/cats.module';
-import { OwnersModule } from './owners/owners.module';
-// import { PhotosModule } from './photos/photos.module';
-// import { UsersModule } from './users/users.module';
+// import { CatsModule } from './cats/cats.module';
+// import { OwnersModule } from './owners/owners.module';
+import { PhotosModule } from './photos/photos.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.DB_URL),
-    CatsModule,
-    OwnersModule,
-    TypeOrmModule.forRoot(),
-    // PhotosModule,
-    // UsersModule,
+    // MongooseModule.forRoot(process.env.DB_URL),
+    // CatsModule,
+    // OwnersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    PhotosModule,
+    UsersModule,
   ],
   controllers: [AppController, IpController, RedirectController],
   providers: [AppService],
